1107c121b15440dc141f5198a1f998a9e783eb86
get-set <535993116@qq.com>
2016-10-19 01:01:34 +0800

    Modify devstack configurations

--- devstack/bootstrap.sh
+++ devstack/bootstrap.sh
@@ -5,13 +5,20 @@ sudo -s << EOF
 apt-get install -y git
 cd /tmp
 git clone https://github.com/openstack-dev/devstack.git
+git checkout -b stable/mitaka origin/stable/mitaka
 bash devstack/tools/create-stack-user.sh
 rm -rf devstack
 
 su - stack << !
 cd ~
-git clone -b stable/mitaka https://github.com/openstack-dev/devstack.git
+mkdir -p .pip
+echo "[global]" > /etc/pip.conf
+echo "index-url = http://pypi.douban.com/simple" >> /etc/pip.conf
+echo "trusted-host = pypi.douban.com" >> /etc/pip.conf
+git clone https://github.com/openstack-dev/devstack.git
+git checkout -b stable/mitaka origin/stable/mitaka
 cp /vagrant/local.conf devstack/
+export GIT_BASE='https://github.com'
 devstack/stack.sh
 !
 
