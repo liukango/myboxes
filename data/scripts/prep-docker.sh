08db7b9a1920373d0b2d41426605cb22c7b541fc
get-set <535993116@qq.com>
2017-10-28 20:14:38 +0800

    Use aliyun accelerator for docker

--- data/scripts/prep-docker.sh
+++ data/scripts/prep-docker.sh
@@ -3,10 +3,22 @@
 sudo -s << EOF
 
 # DaoCloud 安装 Docker
-curl -sSL https://get.daocloud.io/docker | sh
+# curl -sSL https://get.daocloud.io/docker | sh
+uname -a | grep -q ubuntu
+if [ $? = 0 ]; then
+  apt install -y docker.io
+else
+  curl -sSL https://get.daocloud.io/docker > /tmp/install-docker.sh && sh /tmp/install-docker.sh --mirror Aliyun && rm -f /tmp/install-docker.sh
+fi
 
 # DaoCloud 加速器
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://a4e6a79f.m.daocloud.io
-systemctl restart docker || service docker restart
+# curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://a4e6a79f.m.daocloud.io
+# systemctl restart docker || service docker restart
+
+# Aliyun 加速器
+sudo mkdir -p /etc/docker
+sudo echo '{"registry-mirrors": ["https://anwk44qv.mirror.aliyuncs.com"]}' > /etc/docker/daemon.json
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
 EOF
