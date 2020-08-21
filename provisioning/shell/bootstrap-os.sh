cae7ed029fb6c3706d8d9b13f06dede7b1014f65
kang <535993116@qq.com>
2020-08-21 16:54:36 +0800

    Optimize bootstrap-os.sh(no mirror for CentOS)

--- provisioning/shell/bootstrap-os.sh
+++ provisioning/shell/bootstrap-os.sh
@@ -7,11 +7,13 @@ PREINSTALLED_PACKAGES=${1:-"vim"}
 # OS bootstrap:
 # 1. ssh passwordless
 # 2. set timezone
-# 3. set repo mirrors
-# 4. disable selinux
+# 3. disable selinux
+# 4. set locale
+# 5. install necessary packages
 
 sudo -s << EOF
 
+# 1. ssh passwordless
 mkdir -p /root/.ssh && chmod 700 /root/.ssh
 cp /tmp/host.ssh/id_rsa /root/.ssh/ && chmod 600 /root/.ssh/id_rsa
 cp /tmp/host.ssh/id_rsa.pub /root/.ssh/ && chmod 644 /root/.ssh/id_rsa.pub
@@ -21,39 +23,38 @@ sed -i '/PermitRootLogin/c PermitRootLogin yes' /etc/ssh/sshd_config
 sed -i '/PasswordAuthentication/c PasswordAuthentication yes' /etc/ssh/sshd_config
 systemctl restart sshd
 
-
+# 2. set timezone
 timedatectl set-timezone Asia/Shanghai
 
+# 3. disable selinux
+cat /etc/issue | grep -qi "centos" && sed -i '/^SELINUX=/c SELINUX=disabled' /etc/selinux/config && setenforce 0
 
-grep -q "CentOS Linux release 7" /etc/redhat-release && \
-    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup && \
-    curl -sSL http://mirrors.aliyun.com/repo/Centos-7.repo -o /etc/yum.repos.d/CentOS-Base.repo && \
-    rm -f /etc/yum.repos.d/epel* && \
+# 4. set English locale
+echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8" >> /etc/environment
+
+# 5. install necessary packages
+cat /etc/os-release | grep -qi "centos linux 7" && \
+    yum install -y epel-release && \
     curl -sSL http://mirrors.aliyun.com/repo/epel-7.repo -o /etc/yum.repos.d/epel.repo
-
-grep -q "CentOS Linux release 8" /etc/redhat-release && \
-    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup && \
-    curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-8.repo && \
+cat /etc/os-release | grep -qi "centos linux 8" && \
     yum install -y https://mirrors.aliyun.com/epel/epel-release-latest-8.noarch.rpm && \
     sed -i 's|^#baseurl=https://download.fedoraproject.org/pub|baseurl=https://mirrors.aliyun.com|' /etc/yum.repos.d/epel* && \
     sed -i 's|^metalink|#metalink|' /etc/yum.repos.d/epel*
-
 which yum &> /dev/null && \
     echo "Exec: yum makecache ..." && \
     yum makecache &> /dev/null && \
     echo "Installing packages: ${PREINSTALLED_PACKAGES} ..." && \
     yum install -y ${PREINSTALLED_PACKAGES} > /dev/null
 
-which apt &> /dev/null && \
+cat /etc/os-release | grep -qi "Ubuntu" && \
     cp /etc/apt/sources.list /etc/apt/sources.list.bk && \
-    sed -i 's/archive.ubuntu.com/${APT_MIRROR_HOST}/g;s/security.ubuntu.com/${APT_MIRROR_HOST}/g' /etc/apt/sources.list && \
+    sed -i 's/archive.ubuntu.com/${APT_MIRROR_HOST}/g;s/security.ubuntu.com/${APT_MIRROR_HOST}/g' /etc/apt/sources.list
+which apt &> /dev/null && \
     export DEBIAN_FRONTEND=noninteractive && \
     echo "Exec: apt update ..." && \
     apt-get update &> /dev/null && \
     echo "Installing packages: ${PREINSTALLED_PACKAGES} ..." && \
     apt-get -qq install -y ${PREINSTALLED_PACKAGES} > /dev/null
-
-
-cat /etc/issue | grep -qi "centos" && sed -i '/^SELINUX=/c SELINUX=disabled' /etc/selinux/config && setenforce 0
 
 EOF
